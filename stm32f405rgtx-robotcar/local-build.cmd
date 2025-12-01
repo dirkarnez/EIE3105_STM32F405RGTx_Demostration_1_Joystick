ed192073f73dacf8656f7f337d83f6a7df794fed
Dirk Arnez <smalldirkalex@gmail.com>
2025-12-01 18:32:57 +0800

    - update stm32f405rgtx-robotcar/local-build.cmd

--- stm32f405rgtx-robotcar/local-build.cmd
+++ stm32f405rgtx-robotcar/local-build.cmd
@@ -11,4 +11,4 @@ SET PATH=^
 %DOWNLOADS_DIR%\STM32CubeIDE-portable-v1.6.1\plugins\com.st.stm32cube.ide.mcu.externaltools.make.win32_2.1.300.202402091052\tools\bin;^
 %SOFTWARES_DIR%\STM32CubeIDE-portable-v1.6.1\plugins\com.st.stm32cube.ide.mcu.externaltools.make.win32_2.1.300.202402091052\tools\bin;
 
-cd .ci && ( ( make clean && echo =========clean ok========= && make main-build && echo ok ) || echo failed ) && pause
+cd .ci && ( ( make clean && echo =========clean ok========= && make main-build && echo ok ) || ( echo failed && pause ) )
